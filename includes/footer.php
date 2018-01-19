4332a87d75552aa95af67fe2a6a4139c7aa08bcc
Chris Short <devcshort@outlook.com>
2018-01-18 17:06:03 -0800

    Initial Commit
    Initial file/folder structure

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="public/javascripts/jquery-3.2.1.slim.min.js"></script>
+    <script src="public/javascripts/popper.js"></script>
+    <script src="public/javascripts/bootstrap.min.js"></script>
+  </body>
+</html>
