e09a59f582e640a31b3ff4c4eb3fd05b9dc8243e
Chris Short <devcshort@outlook.com>
2018-02-19 09:44:21 -0800

    huge changes to file structure

--- public/includes/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$BRAND_NAME = 'Agency';
--- public/templates/footer.php
+++
@@ -1,9 +0,0 @@
-    <footer>
-      <p class="copyright">&copy; Copyright <?php echo $BRAND_NAME . ' ' . Date('Y'); ?>. All Rights Reserved.</p>
-    </footer>
-    <!-- all javascripts go below -->
-    <script src="public/javascripts/jquery-3.3.1.min.js"></script>
-    <script src="public/javascripts/bootstrap.min.js"></script>
-    <script src="public/javascripts/scripts.js"></script>
-  </body>
-</html>
